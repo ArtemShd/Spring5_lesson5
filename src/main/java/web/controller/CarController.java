7fb03791c8521c4551bc58d90d328a7ba696b875
ArtemShd <artem.shatalov.2016@mail.ru>
2025-10-29 17:52:37 +0300

    Исправлены ошибки

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,10 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import web.model.Car;
 import web.service.CarService;
-
-import java.util.List;
 
 @Controller
 public class CarController {
@@ -21,8 +18,7 @@ public class CarController {
 
     @GetMapping(value = "/cars")
     public String showCars(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        List<Car> cars = carService.getCars(count);
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
